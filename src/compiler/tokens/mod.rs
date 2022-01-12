d24bdb4e9c695f32d0503bb6a7707fcf2d6f5c9b
Adam Bates <adam@adambates.ca>
2022-01-12 00:21:27 -0500

    Added example vec of parsed tokens.

--- src/compiler/tokens/mod.rs
+++ src/compiler/tokens/mod.rs
@@ -1,20 +1,5 @@
-pub struct UnparsedToken {
-  literal: String,
-}
+mod parsed;
+mod unparsed;
 
-impl UnparsedToken {
-  pub fn new<S: Into<String>>(literal: S) -> Self {
-    Self {
-      literal: literal.into(),
-    }
-  }
-}
-
-impl std::fmt::Debug for UnparsedToken {
-  fn fmt(&self, fmt: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
-    write!(fmt, "Unparsed(\"{}\")", self.literal)
-  }
-}
-
-#[derive(Debug)]
-pub struct ParsedToken {}
+pub use parsed::ParsedToken;
+pub use unparsed::UnparsedToken;
---
+++ src/compiler/tokens/unparsed.rs
@@ -0,0 +1,17 @@
+pub struct UnparsedToken {
+  literal: String,
+}
+
+impl UnparsedToken {
+  pub fn new<S: Into<String>>(literal: S) -> Self {
+    Self {
+      literal: literal.into(),
+    }
+  }
+}
+
+impl std::fmt::Debug for UnparsedToken {
+  fn fmt(&self, fmt: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
+    write!(fmt, "Unparsed(\"{}\")", self.literal)
+  }
+}
