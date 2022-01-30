efb0db22ea62a829ef5947f346c7455e47775f34
Adam Bates <adam@adambates.ca>
2022-01-29 23:48:13 -0500

    Added error outputs to executor.

--- src/executor/mod.rs
+++ src/executor/mod.rs
@@ -2,19 +2,28 @@ use super::{config::Config, error::Error};
 use std::process::Command;
 
 pub fn build_and_run(config: &Config) -> Result<(), Error> {
-    Command::new("rustc")
+    let output = Command::new("rustc")
         .arg("-o")
         .arg(&config.output_filepath)
         .arg(&config.build_dir.join(&config.build_filename))
         .output()
         .or_else(|e| Err(Error::new(e.to_string())))?;
 
+    if !output.status.success() {
+        let stderr = output.stderr;
+        let string = String::from_utf8(stderr).or_else(|e| Err(Error::new(e.to_string())))?;
+
+        eprintln!("{}", string);
+
+        return Err(Error::new(string));
+    }
+
     let output = Command::new(&config.output_filepath)
         .output()
-        .or_else(|e| Err(Error::new(e.to_string())))?
-        .stdout;
+        .or_else(|e| Err(Error::new(e.to_string())))?;
 
-    let string = String::from_utf8(output).or_else(|e| Err(Error::new(e.to_string())))?;
+    let stdout = output.stdout;
+    let string = String::from_utf8(stdout).or_else(|e| Err(Error::new(e.to_string())))?;
 
     println!("{}", string);
 
