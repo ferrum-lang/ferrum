199889cfe0570b395a49e236fbaca680297d00e5
Adam Bates <adam@adambates.ca>
2022-01-31 16:14:53 -0500

    Formatted generated rust code.

--- out/build/main.rs
+++ out/build/main.rs
@@ -1,18 +1,15 @@
 mod lang_prelude;
 use lang_prelude::*;
+
 mod lang_std;
-use lang_std::{
-Console,
-};
-fn say_hello(
-name: LangString)
-{
-Console::write_line(LangString::from_owned(format!("Hello, {}!", name)));
+use lang_std::Console;
+
+fn say_hello(name: LangString) {
+  Console::write_line(LangString::from_owned(format!("Hello, {}!", name)));
 }
-pub fn main(
-)
-{
-let name = LangString::from_slice("Adam");
-say_hello(LangString::from_owned(format!("{} Bates", name)));
-say_hello(name);
+
+pub fn main() {
+  let name = LangString::from_slice("Adam");
+  say_hello(LangString::from_owned(format!("{} Bates", name)));
+  say_hello(name);
 }
