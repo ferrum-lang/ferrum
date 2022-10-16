de214921ad08ef1905e8e6baa3d2e45f9cb5f134
adam-bates <adam@adambates.ca>
2022-10-16 12:51:02 -0400

    Removed build files.

--- .ferrum/cargo_gen/src/hello.rs
+++
@@ -1,8 +0,0 @@
-use crate::ferrum::prelude::*;
-
-pub fn say_hello() {
-    inner();
-}
-fn inner() {
-    print(FeStr::from_static("Hello world!"));
-}
--- .ferrum/cargo_gen/src/main.rs
+++
@@ -1,8 +0,0 @@
-mod ferrum;
-use crate::ferrum::prelude::*;
-
-mod hello;
-use crate::hello::say_hello;
-fn main() {
-    say_hello();
-}
