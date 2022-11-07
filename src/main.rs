3b1008cb4bb19fa6bf13bd906bac225b2c9e057e
adam-bates <adam@adambates.ca>
2022-11-07 10:23:37 -0500

    Added some arg parsing to make demoing easier.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,24 @@ use ferrum_oxidize::Result;
 use std::path::PathBuf;
 
 fn main() -> Result {
+    let mut args = std::env::args().skip(1);
+
+    let target_dir = args
+        .next()
+        .unwrap_or_else(|| String::from("."));
+
+    let target_dir = PathBuf::from(target_dir);
+
+    let verbose = match args.next() {
+        Some(arg) if arg.as_str() == "-v" => true,
+        _ => false,
+    };
+
     let config = ferrum_oxidize::Config {
-        entry_file: Some(PathBuf::from("./resources/src/_main.fe")),
-        build_dir: Some(PathBuf::from("./resources/.ferrum/cargo_gen")),
-        out_file: Some(PathBuf::from("./resources/main")),
+        entry_file: Some(target_dir.join(PathBuf::from("src/_main.fe"))),
+        build_dir: Some(target_dir.join(PathBuf::from(".ferrum/cargo_gen"))),
+        out_file: Some(target_dir.join(PathBuf::from("main"))),
+        verbose,
         ..Default::default()
     };
 
@@ -25,8 +39,11 @@ fn main() -> Result {
     let stdout = output.stdout;
     let string = String::from_utf8(stdout)?;
 
-    println!("\n\n*** OUTPUT ***\n{}*** END ***", string);
-    // println!("{string}");
+    if verbose {
+        println!("\n\n*** OUTPUT ***\n{}*** END ***", string);
+    } else {
+        println!("{string}");
+    }
 
     return Ok(());
 }
