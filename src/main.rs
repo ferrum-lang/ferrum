194cf4cdef94d1e5582f4d280e72f93fd1f1f0ef
adam-bates <adam@adambates.ca>
2023-10-02 20:39:06 -0400

    Updated compiler dep

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,16 @@
 use ferrum_compiler::helpers;
 use ferrum_compiler::result::Result;
 
-use std::path::PathBuf;
-// use std::process;
+use std::env;
+
+use env_logger;
 
 fn main() -> Result {
-    let mut args = std::env::args().skip(1);
+    env_logger::init();
 
-    let target_dir = args.next().unwrap_or_else(|| String::from("."));
+    let target_dir = env::args().nth(1).unwrap_or_else(|| String::from("."));
 
-    let target_dir = PathBuf::from(target_dir);
-
-    // let verbose = match args.next() {
-    //     Some(arg) if arg.as_str() == "-v" => true,
-    //     _ => false,
-    // };
-
-    // TODO: support config in compiler
-    // let config = Config {
-    //     entry_file: Some(target_dir.join(PathBuf::from("src/_main.fe"))),
-    //     build_dir: Some(target_dir.join(PathBuf::from(".ferrum/cargo_gen"))),
-    //     out_file: Some(target_dir.join(PathBuf::from("main"))),
-    //     verbose,
-    //     ..Default::default()
-    // };
-
-    let out = helpers::run_full(target_dir)?;
-
-    // let _ = process::Command::new("clear").status()?;
-
-    // println!("{}", String::from_utf8(out.stderr)?);
-    // println!("Output:\n------\n");
+    let out = helpers::run_full(target_dir.into())?;
 
     print!("{}", String::from_utf8(out.stdout)?);
 
